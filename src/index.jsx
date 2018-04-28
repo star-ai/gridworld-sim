d27a65b35e9e31af724d15d0718d2bfabe097fb8
Artem Golubev <artgonline@users.noreply.github.com>
2018-04-28 17:14:23 +1000

    Added react packages and configuration

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const title = 'This is the React app template.';
+
+ReactDom.render(
+  <div>{title}</div>,
+  document.getElementById('app'),
+);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: [
-    path.join(__dirname, 'src', 'index.js'),
+    path.join(__dirname, 'src', 'index'),
   ],
   module: {
     rules: [
