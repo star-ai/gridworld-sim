e85c61cfb95d15ceebea2fb021a97ab83b679640
Artem Golubev <artgonline@users.noreply.github.com>
2018-04-28 15:54:12 +1000

    Added webpack modules and basic condiguration

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+import path from 'path';
+
+module.exports = {
+  entry: [
+    path.join(__dirname, 'src', 'index.js'),
+  ],
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+  },
+};
