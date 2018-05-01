8012a637620f57b39a0232d69d59254403125f2a
Artem Golubev <artgonline@users.noreply.github.com>
2018-05-01 19:42:17 +1000

    Implemented gridworld 2d environment.
    Added tests to cover main functionality of the environment.
    Added jest package, dependencies and configuration.

---
+++ src/lib/envs/gridworld.js
@@ -0,0 +1,59 @@
+export const Actions = Object.freeze({
+  LEFT: 0,
+  UP: 1,
+  RIGHT: 2,
+  DOWN: 3,
+});
+
+export default class Gridworld2D {
+  constructor(shape = [4, 4], finishStates = [0, 15]) {
+    this.finishStates = finishStates;
+    this.gridShape = shape;
+    this.actionsN = Object.keys(Actions).length;
+    this.statesN = shape[0] * shape[1];
+    this.transitions = new Array(this.statesN);
+
+    this.init();
+  }
+
+  getNewState(action, rowNum, colNum) {
+    let newState = -1;
+    if (action === Actions.UP) {
+      newState = (Math.max(0, rowNum - 1) * this.gridShape[0]) + colNum;
+    } else if (action === Actions.DOWN) {
+      newState =
+        (Math.max(rowNum, (rowNum + 1) % this.gridShape[0]) * this.gridShape[0]) + colNum;
+    } else if (action === Actions.LEFT) {
+      newState = (rowNum * this.gridShape[0]) + Math.max(0, colNum - 1);
+    } else {
+      newState = (rowNum * this.gridShape[0]) + Math.max(colNum, (colNum + 1) % this.gridShape[1]);
+    }
+    return newState;
+  }
+
+  init() {
+    for (let i = 0; i < this.gridShape[0]; i++) {
+      for (let j = 0; j < this.gridShape[1]; j++) {
+        const state = (i * this.gridShape[0]) + j;
+        const transition = new Array(this.actionsN);
+        for (const [, action] of Object.entries(Actions)) {
+          const newState = this.getNewState(action, i, j);
+          const done = this.finishStates.includes(state);
+          const reward = done ? 0 : -1;
+          const prob = 1;
+          transition[action] = {
+            prob,
+            newState,
+            reward,
+            done,
+          };
+        }
+        this.transitions[state] = transition;
+      }
+    }
+  }
+
+  sample(state, action) {
+    return this.transitions[state][action];
+  }
+}
---
+++ tests/envs/gridworld2d.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Gridworld2d, { Actions } from '../../src/lib/envs/gridworld';
+
+test('test gridworld2d environment init', () => {
+  const gwEnv = new Gridworld2d();
+  expect(gwEnv.actionsN).toBe(4);
+  expect(gwEnv.statesN).toBe(16);
+  expect(gwEnv.transitions.length).toBe(16);
+});
+
+test('test gridworld2d env transitions', () => {
+  const gwEnv = new Gridworld2d();
+
+  // Transition from left upper conner up
+  let transition = gwEnv.sample(0, Actions.UP);
+  expect(transition.newState).toBe(0);
+  expect(transition.done).toBeTruthy();
+  expect(transition.reward).toBe(0);
+
+  // Transition from left upper conner left
+  transition = gwEnv.sample(0, Actions.LEFT);
+  expect(transition.newState).toBe(0);
+
+  // Transition from left upper conner right
+  transition = gwEnv.sample(0, Actions.RIGHT);
+  expect(transition.newState).toBe(1);
+
+  // Transition from right upper conner right
+  transition = gwEnv.sample(3, Actions.RIGHT);
+  expect(transition.newState).toBe(3);
+
+  // Transition from left bottom conner left
+  transition = gwEnv.sample(12, Actions.LEFT);
+  expect(transition.newState).toBe(12);
+
+  // Transition from left bottom conner up
+  transition = gwEnv.sample(12, Actions.UP);
+  expect(transition.newState).toBe(8);
+
+  // Transition from left bottom conner down
+  transition = gwEnv.sample(12, Actions.DOWN);
+  expect(transition.newState).toBe(12);
+
+  // Transition from right bottom conner RIGHT
+  transition = gwEnv.sample(15, Actions.RIGHT);
+  expect(transition.newState).toBe(15);
+
+  // Transition from middle position up
+  transition = gwEnv.sample(10, Actions.UP);
+  expect(transition.newState).toBe(6);
+  expect(transition.done).toBeFalsy();
+  expect(transition.reward).toBe(-1);
+});
